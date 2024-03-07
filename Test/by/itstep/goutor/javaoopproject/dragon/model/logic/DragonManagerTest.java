58f1bd4a73769e8a66089d704fa2762f7f594e6f
nikishkaa <nikita.goutor@gmail.com>
2024-03-08 01:03:30 +0300

    Add Dragon Manager Test. Update Main class.

---
+++ Test/by/itstep/goutor/javaoopproject/dragon/model/logic/DragonManagerTest.java
@@ -0,0 +1,84 @@
+package by.itstep.goutor.javaoopproject.dragon.model.logic;
+
+import by.itstep.goutor.javaoopproject.dragon.model.data.Dragon;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DragonManagerTest {
+
+    public static Dragon dragon;
+
+    @Before
+    public void init() {
+        dragon = new Dragon();
+        dragon.setName("1");
+    }
+
+    @After
+    public void zeroDragon() {
+        dragon = null;
+    }
+
+
+    @Test
+    public void bornHead() {
+        dragon.setAge(0);
+
+        int expected = 3;
+
+        int actual = DragonManager.calculateDragonHead(dragon);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void invalidAge() {
+        dragon.setAge(-10);
+
+        int expected = -2;
+
+        int actual = DragonManager.calculateDragonHead(dragon);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void firstPeriodHead() {
+        dragon.setAge(100);
+
+        int expected = 303;
+
+        int actual = DragonManager.calculateDragonHead(dragon);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void nullDragon() {
+        dragon = null;
+
+        int expected = -1;
+
+        int actual = DragonManager.calculateDragonHead(dragon);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void dragon200yearsHead() {
+        dragon.setAge(200);
+
+        int expected = 603;
+
+        int actual = DragonManager.calculateDragonHead(dragon);
+
+        assertEquals(expected, actual);
+
+    }
+
+
+
+
+}
--- src/by/itstep/goutor/javaoopproject/dragon/controller/Main.java
+++ src/by/itstep/goutor/javaoopproject/dragon/controller/Main.java
@@ -1,7 +1,14 @@
 package by.itstep.goutor.javaoopproject.dragon.controller;
 
+import by.itstep.goutor.javaoopproject.dragon.model.data.Dragon;
+
 public class Main {
     public static void main(String[] args) {
+
+        Dragon dragon = new Dragon("1", 10);
+
+
+        System.out.println();
 
     }
 }
